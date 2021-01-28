5de31749bc8d433e646f1c2b8c03738167ad54b8
Th3Shadowbroker <webmaster@m4taiori.de>
2021-01-28 20:29:32 +0100

    Implemented TinyServer

---
+++ src/TinyServer.ts
@@ -0,0 +1,64 @@
+import log4js, {Logger} from "log4js";
+import express, {Application} from "express";
+import Configuration, {initialDatabaseConfiguration} from "./util/Configuration";
+import helmet from "helmet";
+import {Connection, createConnection} from "typeorm";
+import {MysqlConnectionOptions} from "typeorm/driver/mysql/MysqlConnectionOptions";
+
+export default class TinyServer {
+
+    private readonly _app: Application;
+
+    private readonly _log: Logger;
+
+    private readonly _config: Configuration;
+
+    private _connection: Connection | undefined;
+
+    private static _instance: TinyServer;
+
+    constructor(config: Configuration) {
+        TinyServer._instance = this;
+
+        this._app = express();
+        this._app.use(helmet());
+
+        this._log = log4js.getLogger(process.env.npm_package_name);
+        this._log.level = process.env.NODE_ENV === "development" ? "debug" : "info";
+
+        this._config = config;
+    }
+
+    async start(): Promise<void> {
+        this._log.info("Initializing database...");
+        const initialConnection = await createConnection(initialDatabaseConfiguration(this._config.database));
+        await initialConnection.query(`CREATE DATABASE IF NOT EXISTS \`${this._config.database.database}\``);
+        await initialConnection.close();
+
+        this._log.info("Creating main connection...")
+        this._connection = await createConnection(this._config.database);
+        const {username, host, port} = <MysqlConnectionOptions>this._connection.options;
+        this._log.info(`Connection established: ${username}@${host}:${port}`);
+
+        this._log.info(`Starting service on port ${this._config.service.port}`);
+        this._app.listen(this._config.service.port);
+        this._log.info("The service is up and running!");
+    }
+
+    get app(): Application {
+        return this._app;
+    }
+
+    get log(): Logger {
+        return this._log;
+    }
+
+    get config(): Configuration {
+        return this._config;
+    }
+
+    static get instance(): TinyServer {
+        return this._instance;
+    }
+
+}
---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import path from "path";
+import dotenv from "dotenv";
+import Configuration, {envOrDefault} from "./util/Configuration";
+import TinyServer from "./TinyServer";
+
+const environment = process.env.NODE_ENV;
+const environmentFile = path.resolve(`./../${environment}.env.local`);
+
+dotenv.config({
+    encoding: "utf-8",
+    path: environmentFile
+});
+
+new TinyServer({
+    service: {
+        port: envOrDefault("SERVICE_PORT", 8080)
+    },
+    database: {
+        type: "mysql",
+        host: envOrDefault("DB_HOST", "localhost"),
+        port: envOrDefault("DB_PORT", 3306),
+        username: envOrDefault("DB_USER", "root"),
+        password: envOrDefault("DB_PASS", ""),
+        database: envOrDefault("DB_NAME", process.env.npm_package_name),
+        synchronize: true,
+        entities: [
+            "./entities/**/*.ts"
+        ],
+        migrations: [
+            "./migrations/**/*.ts"
+        ]
+    }
+}).start()
